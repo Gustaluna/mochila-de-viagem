97fc131bc86016324ad3c97a1525be891274936f
Gustaluna <lgsantana.cin@gmail.com>
2023-02-05 16:44:12 -0300

    final

--- js/main.js
+++ js/main.js
@@ -22,9 +22,9 @@ form.addEventListener("submit", (evento) => {
     
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemetno.id === existe.id)] = itemAtual
     }else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1 : 0
 
         criaElemento(itemAtual)
 
